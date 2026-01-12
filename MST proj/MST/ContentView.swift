661bcb1302e18cedef27abf456e9c986ad581540
peirunwei-amoeba <peirunwei@gmail.com>
2026-01-12 17:18:42 +0800

    start project

---
+++ MST proj/MST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MST
+//
+//  Created by Runwei Pei on 12/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MST proj/MST/MSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSTApp.swift
+//  MST
+//
+//  Created by Runwei Pei on 12/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
