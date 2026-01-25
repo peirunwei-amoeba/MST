6596f3400fe51d22564ade0d7aa99644c73e6e0b
peirunwei-amoeba <peirunwei@gmail.com>
2026-01-25 13:43:37 +0800

    license

--- MST proj/MST/Habit.swift
+++ MST proj/MST/Habit.swift
@@ -2,7 +2,14 @@
 //  Habit.swift
 //  MST
 //
-//  Created by Claude on 1/20/26.
+//  Copyright © 2025 Pei Runwei. All rights reserved.
+//
+//  This Source Code Form is subject to the terms of the PolyForm Strict
+//  License 1.0.0. You may not use, modify, or distribute this file except
+//  in compliance with the License. A copy of the License is located at:
+//  https://polyformproject.org/licenses/strict/1.0.0
+//
+//  Required Notice: Copyright Pei Runwei (https://github.com/peirunwei-amoeba)
 //
 
 import Foundation
